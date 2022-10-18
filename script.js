f162542dd0e43c07d3be46b02cee57c3f8759a68
Victor <holmstrom.victor@gmail.com>
2022-10-18 21:16:37 +0300

    Add functionality to create different sized grids and remove the previous one

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-const gridRows = 10;
-const gridSize = gridRows * gridRows;
 const container = document.querySelector("#grid-container");
-const basis = 100/gridRows;
+const resetButton = document.querySelector('#reset-button');
+let gridFull = false;
 
-
-for (let i = 0; i < gridSize; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.flexBasis = basis + "%";
-    container.appendChild(square);
+function renderGrid (size, basis) {
+    for (let i = 0; i < size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.flexBasis = basis + "%";
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        })
+        container.appendChild(square);
+    }
 }
 
-const squares = document.querySelectorAll('.square');
+function createGrid (rows) {
+    const gridSize = rows * rows;
+    const squareBasis = 100/rows;
+    renderGrid(gridSize, squareBasis);
+    gridFull = true;
+}
 
-squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = "black";
-    })
+function emptyGrid () {
+    const squares = document.querySelectorAll(".square");
+    for (i = 0; i < squares.length; i ++) {
+        container.removeChild(squares[i]);
+    }
+    gridFull = false;
+}
+
+function newGrid () {
+    if (gridFull) {
+        emptyGrid();
+    }
+    let userInput = parseInt(prompt("How many squares per side?"));
+    if (userInput <= 100 && userInput >= 1) {
+        createGrid(userInput);
+    } else {
+        alert("Invalid number");
+        return;
+    }
+}
+
+createGrid(10);
+
+resetButton.addEventListener('click', () => {
+    newGrid();
 });

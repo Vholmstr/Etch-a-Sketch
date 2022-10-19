bafc773babea7fee068a56715ac95801bb3f2e9c
Victor <holmstrom.victor@gmail.com>
2022-10-19 23:41:30 +0300

    add functionality to black-white button and rainbow mode button, also add button to empty board

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const container = document.querySelector("#grid-container");
 const resetButton = document.querySelector('#reset-button');
 const rainbowmodeButton = document.querySelector('#rainbow-mode');
+const blackWhiteModeButton = document.querySelector('#black-white-mode');
+const darkenModeButton = document.querySelector('#darken-mode');
+const emptyGridButton = document.querySelector('#empty-grid-button');
+
 let gridFull = false;
-let rainbowmode = false;
+let blackWhiteMode = true;
+let rainbowMode = false;
+let darkenMode = false;
 
 function renderGrid (size, basis) {
     for (let i = 0; i < size; i++) {
@@ -10,11 +16,16 @@ function renderGrid (size, basis) {
         square.classList.add('square');
         square.style.flexBasis = basis + "%";
         square.addEventListener('mouseover', () => {
-            if (rainbowmode) {
+            if (rainbowMode && !square.style.backgroundColor.includes("rgb")) {
                 const rgbColor = RandomRGB();
                 square.style.backgroundColor = `rgb(${rgbColor[0]},${rgbColor[1]},${rgbColor[2]})`
-            } else {
+            } else if (blackWhiteMode) {
                 square.style.backgroundColor = "black"; 
+            } else if (darkenMode) {
+                const color = square.style.backgroundColor;
+                console.log(color);
+            } else {
+                return;
             }
         })
         container.appendChild(square);
@@ -57,8 +68,40 @@ function RandomRGB() {
     return [r,g,b];
 }
 
+/* function darkenByTenPercent (color) {
+
+    const darkenedColor =  `rgb(${color},${color},${color})`;
+    return darkenedColor;
+}
+ */
+
 createGrid(10);
 
 resetButton.addEventListener('click', () => {
     newGrid();
 });
+
+blackWhiteModeButton.addEventListener('click', () => {
+    rainbowMode = false;
+    darkenMode = false;
+    blackWhiteMode = true;
+})
+
+rainbowmodeButton.addEventListener('click', () => {
+    rainbowMode = true;
+    darkenMode = false;
+    blackWhiteMode = false;
+})
+
+darkenModeButton.addEventListener('click', () => {
+    rainbowMode = false;
+    darkenMode = true;
+    blackWhiteMode = false;
+})
+
+emptyGridButton.addEventListener('click', () => {
+    const squares = document.querySelectorAll(".square");
+    for (i = 0; i < squares.length; i ++) {
+        squares[i].style.backgroundColor = "white";
+    }
+})
